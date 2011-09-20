0abf9ef838c0d657bac1dcc5fdd8c81af1f43e98
flamingcow@google.com <flamingcow@google.com@960bbd1a-3921-0410-93bb-a35bc832efea>
2011-09-20 20:23:38 +0000

    Trickle (rate-limited database operations) tool and library.
    git-svn-id: http://google-mysql-tools.googlecode.com/svn/trunk@300 960bbd1a-3921-0410-93bb-a35bc832efea

---
+++ pylib/trickle_lib.py
@@ -0,0 +1,232 @@
+#!/usr/bin/python2.6
+#
+# Copyright 2006 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""A library to allow for long running, slow operations.
+
+Intended mainly for database operations, this library provides a
+skeleton to safely execute a given set of operations such that the
+runtime is a fraction of realtime (such as N% utilization).  It is
+designed to keep track of its position and be restartable, though that
+is dependent upon the class that subclasses TrickledOperation.
+"""
+
+__author__ = 'bbiskebo@google.com (Brian Biskeborn)'
+# Original author: chip@google.com (Chip Turner)
+
+import logging
+import time
+
+
+class TrickledOperation(object):
+  """The class representing both how a trickle operation is performed and
+  the state associated with a given operation.
+
+  Users of this class will subclass it and implement the protected
+  methods _SetupTrickle, _Finished, _PerformTrickle, and
+  _FinalizeTrickle.  These methods will be called during a trickle
+  operation such that the size of the batch passed to _PerformTrickle
+  tries to be exactly utilization_percent of each cycle_time.
+
+  Specifying utilization_percent=100 turns off all sleeping, and
+  does the work at maximum speed.
+  """
+  def __init__(self, utilization_percent, cycle_time):
+    """Constructor.  Subclass and store operation-specific state here.
+
+    Args:
+      utilization_percent: percent of time to spend in _PerformTrickle
+      cycle_time: interval over which utilization_percent is
+                  calculated per run
+    """
+    self._utilization_fraction = utilization_percent / 100.0
+    self._cycle_time = cycle_time
+    self._batch_size = 1    # Start maximally cautious (single statement)
+                            # and increase batch size as we can
+    self._batch_size_limit = 1000000
+    self.verbose = True
+
+  def _SetupTrickle(self):
+    """A setup method, invoked before a trickle loop is run.
+
+    It is valid to execute multiple loops, and this method will be
+    invoked for each.  Useful for finding the next starting position.
+    """
+    raise NotImplementedError("call to pure virtual function")
+
+  def _FinalizeTrickle(self):
+    """A completion method copied after a series of trickle loops.
+
+    Intended to finish up state, such as the final 'slosh' rows of a
+    continual copy loop.
+    """
+    raise NotImplementedError("call to pure virtual function")
+
+  def _Finished(self):
+    "Called to determine if a trickle is complete."
+    raise NotImplementedError("call to pure virtual function")
+
+  def _PerformTrickle(self, batch_size):
+    """The method invoked to perform the actual trickle operation.
+
+    This method will be invoked multiple times and is passed the size
+    of a batch to execute, which will vary depending upon the runtime
+    of the previous batch.
+
+    Args:
+      batch_size: size of the current batch (subclass dependent)
+
+    Returns:
+      number of items processed (usually batch_size)
+    """
+    raise NotImplementedError("call to pure virtual function")
+
+  def _GetProgress(self):
+    """Called to fetch progress information
+
+    This method is intended to be overridden by inheriting classes
+    that can provide data about their progress through the trickle.
+
+    Args:
+      none
+    Returns:
+      String representation of current progress state or None if unknown
+    """
+    return None
+
+  def SetBatchSizeLimit(self, n):
+    """Set the maximum batch_size that will ever be submitted by Trickle().
+
+    Intended for operations that need to be artificially slowed down in
+    excess of what the throttler would have done.  For instance, when
+    replicas are drastically slower than the primary for some reason, which
+    the throttler will be unable to detect.
+
+    Args:
+      n - how many queries may be run in a batch.  Values above
+          1000000 are probably ill-advised.
+    """
+    self._batch_size_limit = n
+
+  def Trickle(self):
+    """Perform the actual trickle operation.
+
+    This function will loop until self._Finished() returns true.  It
+    calls the above methods in this order:
+
+    self._SetupTrickle()
+    while not self._Finished():
+      ...
+      self._PerformTrickle(self, batch_size)
+      ...
+    self._FinalizeTrickle()
+
+    Args:
+      None
+    Returns:
+      None
+    """
+    self._SetupTrickle()
+
+    # track the last ten cycles worth of copy rates
+    copy_rates = [0] * 10
+    copy_rate_idx = 0
+
+    # also track average copies over this invocation's lifetime
+    rows_copied = 0
+    start_time = time.time()
+
+    while not self._Finished():
+      then = time.time()
+      batch_size = self._batch_size
+
+      rowcount = self._PerformTrickle(batch_size)
+
+      # Increase or decrease batch size to get the target utilization
+      # percentage at the given cycle time. At all times, we're cautious about
+      # floating point rounding effects and the like.
+      time_delta = time.time() - then
+      ideal_delta = self._cycle_time * self._utilization_fraction
+
+      if time_delta > 2.0 * ideal_delta:
+        # If our utilization fraction is way too high, we want to decrease our
+        # batch size fairly drastically, but we don't want to chop things to
+        # nothing in case what we saw was a transient blip. Note that if we get
+        # here, either (a) we've seen enough short statements to increase our
+        # batch size from its initial value of 1 before hitting this, or (b) a
+        # batch size of 1 is still way too big, in which case we modulate cycle
+        # time below instead.
+        self._batch_size = max(int(batch_size/2), 1)
+      elif time_delta * 1.5 < ideal_delta:
+        # We don't want to increase batch size quite as drastically
+        self._batch_size = max(int(batch_size * 1.5), batch_size + 1)
+      else:
+        # batch_size is between 2x and 0.67x target. Modulate it directly to
+        # the target value.
+        self._batch_size = max(int(batch_size * ideal_delta / time_delta), 1)
+
+      # Rev limiter in case an operation is a no-op by accident.
+      self._batch_size = min(self._batch_size, self._batch_size_limit)
+
+      # How long are we going to sleep this time? At least the rest of the
+      # cycle time, longer if utilization would otherwise be too high, and
+      # at least one second regardless but no more than 2x the cycle time.
+      if self._utilization_fraction < 1.0:
+        sleep_time = min(
+            max(self._cycle_time - time_delta,
+                (time_delta / self._utilization_fraction) - time_delta,
+                1),
+            2 * self._cycle_time)
+      else:
+        # But if running with 100% utilization, don't sleep at all.
+        sleep_time = 0.0
+
+      # update average copy rate
+      this_batch_rate = batch_size / (sleep_time + time_delta)
+      copy_rates[copy_rate_idx] = this_batch_rate
+      copy_rate_idx = (copy_rate_idx + 1) % len(copy_rates)
+      current_rate_avg = sum(copy_rates) / float(len(copy_rates))
+      rows_copied += rowcount
+
+      if self.verbose:
+        self._LogStatus(batch_size, start_time, time_delta, sleep_time,
+                        current_rate_avg, rows_copied, copy_rate_idx)
+
+      time.sleep(sleep_time)
+
+    if self.verbose:
+      self._LogFinish(rows_copied, start_time)
+    self._FinalizeTrickle()
+
+  def _LogStatus(self, batch_size, start_time, time_delta, sleep_time,
+                 current_rate_avg, rows_copied, copy_rate_idx):
+    progress = self._GetProgress()
+    if progress:
+      progress = ', ' + progress
+    else:
+      progress = ''
+
+    logging.info("batch of %d in %.2f s%s, sleeping %.2f s"
+                 % (batch_size, time_delta, progress, sleep_time))
+    logging.info("util %.2f, new batch size %d "
+                 "(%.2f current, %.2f avg rows/sec)"
+                 % (time_delta/(time_delta+sleep_time),
+                    self._batch_size,
+                    current_rate_avg,
+                    rows_copied/(time.time() - start_time)))
+
+  def _LogFinish(self, rows_copied, start_time):
+    logging.info("Done: %.2f avg rows/sec",
+                 (rows_copied / (time.time() - start_time)))
---
+++ trickle_file_runner.py
@@ -0,0 +1,318 @@
+#!/usr/bin/python2.6
+#
+# Copyright 2006 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""A tool for applying a file of SQL statements to a database.
+
+Usage: trickle_file_runner --dbspec host:user:pass:db --input FILENAME \\
+                         [ --state_database DB ] [ --state_table TABLENAME ] \\
+                         [ --utilization_percent D ] [ --cycle_time C ]
+
+This utility will take a given file of SQL statements and apply them
+to a database in such a way as to guarantee they are applied in order,
+even across restarts.  It also guarantees no statement will be
+executed twice or skipped.  It will throttle itself, depending upon
+cycle time and utilization percent.
+
+When executing the given file, groups of statements in the SQL file
+will be merged into transactions whose size varies depending on the
+trickle rate.  If the file does not contain BEGIN/COMMIT blocks, then
+each line is considered a standalone statement.  If it does contain
+such blocks, then those blocks will be executed in their entirety
+(though they will likely be grouped in with other such blocks inside
+of a transaction).
+"""
+
+__author__ = 'bbiskebo@google.com (Brian Biskeborn)'
+# Original author: chip@google.com (Chip Turner)
+
+import hashlib
+import logging
+import re
+
+from pylib import app
+from pylib import db
+from pylib import flags
+from pylib import trickle_lib
+
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string("state_database", "admin",
+                    "database name to store state table in (i.e., 'scratch')")
+flags.DEFINE_string("state_table", "TrickleFileState",
+                    "table to store state in")
+flags.DEFINE_string("dbspec", "", "database to execute statements in")
+flags.DEFINE_string("input", "", "filename to process as input")
+flags.DEFINE_integer("cycle_time", 5,
+                     "seconds each batch of statements should take")
+flags.DEFINE_integer("utilization_percent", 10,
+                     "target time per cycle to be executing statements rows")
+flags.DEFINE_integer("artificial_batch_cap", None,
+                     "never run more than this many lines per batch")
+flags.DEFINE_boolean("init_state_table", False,
+                     "populate the state table with file checksums and exit")
+flags.DEFINE_boolean("allow_warnings", False,
+                     "continue after sql statements that give warnings")
+flags.DEFINE_string("session_init", None,
+                    "SQL commands to run to initialize the session, "
+                    "e.g. SET LOG_BIN=0")
+
+# regexes we use to help identify transaction blocks
+valid_sql_re = re.compile(
+    r'(INSERT (IGNORE )?INTO|REPLACE INTO|UPDATE|DELETE)\s', re.IGNORECASE)
+begin_re = re.compile("^BEGIN;?\s*$", re.IGNORECASE)
+commit_re = re.compile("^COMMIT;?\s*$", re.IGNORECASE)
+
+
+def ValidSqlLine(line):
+  """Verify that a given line of text contains valid SQL for the filerunner.
+
+  For our purposes, valid means one of:
+  BEGIN, COMMIT, INSERT INTO, INSERT IGNORE INTO, REPLACE INTO,
+  DELETE FROM, UPDATE.
+
+  Args:
+    line: string to validate
+  Returns:
+    Boolean, True if the line is valid.
+  """
+  return (valid_sql_re.match(line) or
+          begin_re.match(line) or
+          commit_re.match(line))
+
+
+def _PrepareFile(filename):
+  """Open a file, validate it and compute some identifying information.
+
+  Args:
+    filename: file to work on
+  Returns:
+    A file handle, the file size, and the file's SHA1 checksum.
+    The file handle is positioned at end of file.
+  """
+  fh = open(filename, 'r')
+  checksummer = hashlib.sha1()
+  filesize = 0
+
+  for line in fh:
+    if not ValidSqlLine(line):
+      logging.fatal("File contains invalid sql: %s", line)
+
+    filesize += len(line)
+    checksummer.update(line)
+
+  return fh, filesize, checksummer.hexdigest()
+
+
+def InitStateTable(dbh, state_db, state_table, fh, fsize, fchecksum):
+  """Add file information to the state table.
+
+  If a row already exists for the specified file name, that row is replaced.
+  """
+  dbh.ExecuteOrDie("REPLACE INTO %s.%s VALUES ('%s', %d, '%s', 0)" %
+                   (state_db, state_table, fh.name, fsize, fchecksum))
+
+
+class DbFileTrickler(trickle_lib.TrickledOperation):
+  """A class to represent trickling a file of SQL statements into a database."""
+
+  def __init__(self, dbh, utilization_percent, cycle_time,
+               state_database, state_table, fh, size, checksum):
+    """Constructor. Use _PrepareFile() to get the file handle/size/checksum.
+
+    Args:
+      dbh: pylib.db handle to the database
+      utilization_percent: percent of database time to try to use
+      cycle_time: duration of a cycle of insert/sleeps
+      state_database: database name to store the state table in
+      state_table: table name to store our state in
+      fh: handle to the file to trickle statements from
+      size: size of the open file in bytes
+      checksum: checksum of the open file
+    """
+    trickle_lib.TrickledOperation.__init__(self, utilization_percent,
+                                           cycle_time)
+
+    self._db = dbh
+    self._filename = fh.name
+    self._fh = fh
+    self._size = size
+    self._checksum = checksum
+    self._offset_bytes = 0
+    self._state_database = state_database
+    self._state_table = state_table
+    self._VerifyStateDatabase()
+
+  def _GetProgress(self):
+    """Report our progress for use in status messages."""
+    return "%d%% done" % (self._offset_bytes * 100 / self._size)
+
+  def _VerifyStateDatabase(self):
+    """Check that the state table exists and create an entry for our input file.
+
+    If an entry for the input file already exists, verify its size and
+    checksum.
+    """
+    rows = self._db.ExecuteOrDie(
+        "SELECT Checksum, Size, Offset FROM %s.%s WHERE Filename = '%s'" %
+        (self._state_database, self._state_table, self._filename))
+
+    if not rows:
+      logging.info("Creating row in state table")
+      self._db.ExecuteOrDie('BEGIN')
+      self._db.ExecuteOrDie(
+          "INSERT INTO %s.%s VALUES ('%s', %d, '%s', 0)" %
+          (self._state_database, self._state_table, self._filename,
+           self._size, self._checksum))
+      self._db.ExecuteOrDie('COMMIT')
+    else:
+      if self._size != long(rows[0]['Size']):
+        logging.fatal("database filesize does not match actual file: %s vs %s",
+                      self._size, rows[0]['Size'])
+      if self._checksum != rows[0]['Checksum']:
+        logging.fatal("SHA-1 checksum mismatch on file vs database")
+      self._offset_bytes = rows[0]['Offset']
+      logging.info("Resuming at offset %d", self._offset_bytes)
+
+  def _SetupTrickle(self):
+    """Determine the current file offset based on our state table.
+
+    Called once before the first call to _PerformTrickle().
+    """
+    row = self._db.ExecuteOrDie(
+        "SELECT Offset FROM %s.%s WHERE Filename = '%s'" %
+        (self._state_database, self._state_table, self._filename))
+    assert len(row) == 1 and len(row[0]) == 1
+
+    self._offset_bytes = long(row[0]['Offset'])
+
+  def _FinalizeTrickle(self):
+    pass
+
+  def _Finished(self):
+    """Have we finished processing the file?"""
+    return self._offset_bytes == self._size
+
+  def _PerformTrickle(self, batch_size):
+    """Execute batch_size blocks and update the state table in a transaction.
+
+    Args:
+      batch_size: number of blocks to execute. A block is either a single line
+                  in the input file, or a group of lines between BEGIN/COMMIT
+                  statements.
+    Returns:
+      Number of blocks actually executed.
+    """
+    starting_offset = self._offset_bytes
+    self._fh.seek(self._offset_bytes)
+
+    # pre-read the data to avoid interleaving disk reads with database writes.
+    #
+    # also, break the input file into batches.  if we see a BEGIN then
+    # we must read through to the next COMMIT even if we would be
+    # bigger than batch size.
+
+    # batch is a list of lists.  each entry represents a transaction
+    # (ie, list of statements) to be applied to the database.
+    batch = []
+    while len(batch) < batch_size:
+      line = self._fh.readline()
+      if not line:
+        break
+
+      if not ValidSqlLine(line):
+        logging.fatal("Encountered invalid sql: %s", line)
+
+      if begin_re.match(line):
+        transaction = []
+        while 1:
+          next_line = self._fh.readline()
+
+          if not next_line:
+            logging.fatal("Input file terminated inside of BEGIN block")
+          elif not ValidSqlLine(next_line):
+            logging.fatal("Encountered invalid sql: %s", next_line)
+          elif begin_re.match(next_line):
+            logging.fatal("Attempt to nest transactions")
+          elif commit_re.match(next_line):
+            break
+          else:
+            transaction.append(next_line)
+        batch.append(transaction)
+      elif commit_re.match(line):
+        logging.fatal("Attempt to commit outside of transaction")
+      else:
+        batch.append([line])
+
+    self._offset_bytes = self._fh.tell()
+    self._db.ExecuteOrDie('BEGIN')
+
+    for transaction in batch:
+      for statement in transaction:
+        try:
+          self._db.ExecuteOrDie(statement)
+        except db.QueryWarningsException, e:
+          logging.warn('SQL generated warning: %s, %s', str(e), statement)
+          if not FLAGS.allow_warnings:
+            raise
+
+    result = self._db.ExecuteOrDie(
+        "UPDATE %s.%s SET Offset = %d WHERE Filename = '%s' AND Offset = %d" %
+        (self._state_database, self._state_table,
+         self._offset_bytes, self._filename, starting_offset))
+    if result.GetRowsAffected() != 1:
+      logging.fatal("Attempt to update database state but something "
+                    "already changed it")
+    self._db.ExecuteOrDie('COMMIT')
+
+    return len(batch)
+
+
+def main(unused_argv):
+  if not (FLAGS.state_database and FLAGS.dbspec and FLAGS.input):
+    app.usage(shorthelp=1)
+    return 1
+
+  logging.info('Checking file %s', FLAGS.input)
+  fh, size, checksum = _PrepareFile(FLAGS.input)
+
+  dbspec = db.Spec.Parse(FLAGS.dbspec)
+  if not dbspec.IsSingle():
+    logging.fatal('Must specify a single database shard to operate on')
+  with dbspec.Connect() as dbh:
+    if FLAGS.session_init:
+      logging.info('Initializing session with: %s', FLAGS.session_init)
+      dbh.ExecuteOrDie(FLAGS.session_init)
+
+    if FLAGS.init_state_table:
+      logging.info('Setting up the state table')
+      InitStateTable(dbh, FLAGS.state_database, FLAGS.state_table,
+                     fh, size, checksum)
+    else:
+      logging.info('Starting the trickle')
+      trickler = DbFileTrickler(dbh, FLAGS.utilization_percent,
+                                FLAGS.cycle_time, FLAGS.state_database,
+                                FLAGS.state_table, fh, size, checksum)
+      if FLAGS.artificial_batch_cap:
+        trickler.SetBatchSizeLimit(FLAGS.artificial_batch_cap)
+      trickler.Trickle()
+
+  logging.info('Done')
+
+
+if __name__ == '__main__':
+  FLAGS.logbuflevel = -1
+  app.run()
