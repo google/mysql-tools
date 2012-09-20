ea9b1b8e3c6eb695a81f278f56b73c02d131bcb8
flamingcow@google.com <flamingcow@google.com@960bbd1a-3921-0410-93bb-a35bc832efea>
2012-09-20 20:20:27 +0000

    HTTP server framework for google-mysql code.
    git-svn-id: http://google-mysql-tools.googlecode.com/svn/trunk@326 960bbd1a-3921-0410-93bb-a35bc832efea

---
+++ pylib/http_handlers.py
@@ -0,0 +1,301 @@
+#!/usr/bin/python2.6
+#
+# Copyright 2012 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Standard HTTP request handlers for an HTTP server."""
+
+__author__ = 'mqian@google.com (Mike Qian)'
+
+
+import gc
+import linecache
+import logging
+import os
+import re
+import sys
+import threading
+import traceback
+
+import db
+import vars_common
+import vars_export
+import vars_helper
+
+
+_HTML_HEADER = """<HTML>
+<HEAD><TITLE>%s</TITLE></HEAD>
+<BODY>
+"""
+
+
+_HTML_FOOTER = """
+</BODY>
+</HTML>
+"""
+
+
+_HTMLDOC_STYLE = """<style>
+span {
+  position: relative;
+  cursor: hand;
+}
+span b {
+  text-decoration: none;
+  color: #CC3300;
+}
+span div {
+  display: none;
+  z-index: 999;
+  position: absolute;
+  top: 2em;
+  left: 80px;
+  border: 1px solid #3333CC;
+  padding: 1px;
+  background-color: #CCCCFF;
+  font-size: smaller;
+  width: 400;
+}
+span div:first-line {
+  text-decoration: underline;
+}
+span:hover div {
+  display: block;
+}
+body {
+  padding-bottom: 100px;
+}
+</style>
+"""
+
+
+def HandleAbortAbortAbort(request, unused_response):
+  """Handles /abortabortabort requests."""
+  logging.error('/abortabortabort from %s' % request.remote_addr)
+  os.abort()
+
+
+def HandleDBs(unused_request, response):
+  """Renders /dbs page with database connection usage."""
+  response.headers.add_header('Content-Type', 'text/plain')
+  response.start_response(200)
+
+  for conn in db.CONNECTIONS:
+    response.out.write(str(conn()))
+    response.out.write('\n')
+
+  response.end_response()
+
+
+def HandlePyHeaps(unused_request, response):
+  """Renders /pyheaps page with Python object memory usage."""
+  response.headers.add_header('Content-Type', 'text/plain')
+  response.start_response(200)
+
+  count = {}
+  mem_used = {}
+  for obj in gc.get_objects():
+    count[type(obj)] = count.get(type(obj), 0) + 1
+    mem_used[type(obj)] = mem_used.get(type(obj), 0) + sys.getsizeof(obj)
+
+  response.out.write('Python objects (total bytes, count, type):\n\n')
+  for total_size, type_ in sorted(((v, k) for k, v in mem_used.iteritems()),
+                                  reverse=True):
+    response.out.write('%12s %12s  %s\n' % (total_size, count[type_], type_))
+
+  response.end_response()
+
+
+def HandlePyThreads(unused_request, response):
+  """Renders /pythreads page with Python thread stacks."""
+  response.headers.add_header('Content-Type', 'text/plain')
+  response.start_response(200)
+
+  frames = sys._current_frames()
+  threads = [(t.ident, t) for t in threading.enumerate()]
+  thread_names = dict((tid, '(name: %s) ' % t.getName()) for tid, t in threads)
+
+  response.out.write('Python threads:\n\n')
+  for thread_id, frame in frames.items():
+    response.out.write('--- Thread %d %sstack: ---\n' %
+                       (thread_id, thread_names.get(thread_id, '')))
+    frame_tuples = []
+    while frame:
+      filename = frame.f_code.co_filename
+      lineno = frame.f_lineno
+      line = linecache.getline(filename, lineno)
+      frame_tuples.append((filename, lineno, frame.f_code.co_name, line))
+      frame = frame.f_back
+    frame_tuples.reverse()
+    response.out.write(''.join(traceback.format_list(frame_tuples)))
+  response.end_response()
+
+
+def HandleQuitQuitQuit(request, response, server):
+  """Handles /quitquitquit requests."""
+  response.headers.add_header('Content-Type', 'text/plain')
+  response.start_response(200)
+  response.out.write('Shutting down...')
+  response.end_response()
+  logging.warn('/quitquitquit from %s', request.remote_addr)
+  server.Shutdown()
+
+
+def HandleVars(request, response):
+  """Renders /vars page with exported variables."""
+  requested_vars = request.get('var', '')
+  output = request.get('output', 'html')
+  regexes = [re.compile(x) for x in request.get('varregexp', [])]
+
+  if regexes and requested_vars:
+    title = 'Request Error'
+    error_msg = (
+        'ERROR: Can only specify one of "var=" or "varregexp=" parameters')
+    response.headers.add_header('Content-Type', 'text/html')
+    response.start_response(200)
+    response.out.write(_HTML_HEADER % title + error_msg + _HTML_FOOTER)
+    response.end_response()
+    return
+
+  values = {}
+  title = 'All Variables'
+  # Filter by specific vars.
+  if requested_vars:
+    vars_list = requested_vars.split(':')
+    if len(vars_list) == 1:
+      title = 'Variable: %s' % requested_vars.strip()
+    else:
+      title = 'Variables: %s' % requested_vars.strip()
+    for var in vars_list:
+      name = vars_common.CanonicalName(var)
+      value = vars_helper.FetchVar(var)
+      if value is None:
+        values[name] = '(Variable not found)'
+      else:
+        values[name] = value
+  # Filter vars by regexes.
+  elif regexes:
+    title = 'Matching Variables'
+    for k, v in vars_helper.FetchAllVars().iteritems():
+      for regex in regexes:
+        if regex.match(k):
+          values[k] = v
+          break
+  # Just show all the vars.
+  else:
+    values = vars_helper.FetchAllVars()
+
+  callback = _OUTPUT_FORMATS.get(output, _OUTPUT_FORMATS['html'])
+  callback(response, values, title)
+
+
+def HandleVarsDoc(request, response):
+  """Renders /varsdoc page with documentation for exported variables."""
+  title = 'All Variables'
+  if request.get('output', '') == 'text':
+    response.headers.add_header('Content-Type', 'text/plain; charset=UTF-8')
+    response.start_response(200)
+    response.out.write(_HTML_HEADER % title)
+    response.out.write(vars_export.GetAllDocPlain())
+    response.out.write(_HTML_FOOTER)
+    response.end_response()
+  else:
+    response.headers.add_header('Content-Type', 'text/html; charset=UTF-8')
+    response.start_response(200)
+    response.out.write(_HTML_HEADER % title)
+    response.out.write(vars_export.GetAllDocHTML())
+    response.out.write(_HTML_FOOTER)
+    response.end_response()
+
+
+def _WritePlainResponse(response, values, unused_title):
+  """Write the HTTP response for a /vars?output=text request.
+
+  Args:
+    response: The HTTP response object.
+    values: A dictionary containing (name, value) mappings to be written.
+  """
+  content = ''.join(vars_export.GetPairPlain(key, values[key])
+                    for key in values)
+  if len(values) == 1:
+    value = content.split(' ', 1)[1]
+    if not value.startswith('map:'):
+      content = value
+  response.headers.add_header('Content-Type', 'text/plain; charset=UTF-8')
+  response.start_response(200)
+  response.out.write(content)
+  response.end_response()
+
+
+def _WriteHTMLResponse(response, values, title='All Variables'):
+  """Write the HTTP response for a /vars?output=html request.
+
+  Args:
+    title: The page title.
+    response: The HTTP response object.
+    values: A dictionary containing (name, value) mappings to be written.
+  """
+  content = ''.join(vars_export.GetPairHTML(key, values[key]) for key in values)
+  if len(values) == 1:
+    value = content.split(' ', 1)[1]
+    if not value.startswith('map:'):
+      content = value
+  response.headers.add_header('Content-Type', 'text/html; charset=UTF-8')
+  response.start_response(200)
+  response.out.write(_HTML_HEADER % title)
+  response.out.write('<tt>\n' + content + '</tt>')
+  response.out.write(_HTML_FOOTER)
+  response.end_response()
+
+
+def _WriteHTMLDocResponse(response, values, title='All Variables'):
+  """Write the HTTP response for a /vars?output=htmldoc request.
+
+  Args:
+    title: The page title.
+    response: The HTTP response object.
+    values: A dictionary containing (name, value) mappings to be written.
+  """
+  content = ''.join(vars_export.GetVarDocHTML(var) for var in values)
+  response.headers.add_header('Content-Type', 'text/html; charset=UTF-8')
+  response.start_response(200)
+  response.out.write(_HTML_HEADER % title)
+  response.out.write(_HTMLDOC_STYLE)
+  response.out.write('<tt>\n' + content + '</tt>')
+  response.out.write(_HTML_FOOTER)
+  response.end_response()
+
+
+STANDARD_HANDLERS = {
+    '/abortabortabort': HandleAbortAbortAbort,
+    '/dbs': HandleDBs,
+    '/pyheaps': HandlePyHeaps,
+    '/pythreads': HandlePyThreads,
+    '/quitquitquit': HandleQuitQuitQuit,
+    '/vars': HandleVars,
+    '/varsdoc': HandleVarsDoc,
+}
+
+
+_OUTPUT_FORMATS = {
+    'text': _WritePlainResponse,
+    'html': _WriteHTMLResponse,
+    'htmldoc': _WriteHTMLDocResponse,
+}
+
+
+def StandardHandlers(callback, request, response, *args, **kwargs):
+  """Handles standard page endpoints, or calls callback."""
+  uri = request.path.rstrip('/')
+  STANDARD_HANDLERS.get(uri, callback)(request, response, *args, **kwargs)
---
+++ pylib/http_server.py
@@ -0,0 +1,278 @@
+#!/usr/bin/python2.6
+#
+# Copyright 2012 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Basic HTTP server API.
+
+This API is a hybrid of WSGI and WebOb APIs. It's been modified to make
+responding to a request an asynchronous operation, so the application doesn't
+have to block a thread if it needs to query a backend.
+
+Example usage:
+
+def Callback(request, response):
+  response.add_header('Content-Type', 'text/plain')
+  response.start_response(200)
+  response.out.write('Your IP address is: %s' % request.remote_addr)
+  response.end_response()
+
+server = http_server.Server('0.0.0.0', 9999, Callback)
+server.Loop()
+"""
+
+import asyncore
+import logging
+import socket
+import threading
+import urlparse
+import weakref
+from wsgiref import headers
+
+
+class CaseInsensitiveDict(dict):
+  """Dictionary in which keys are case-insensitive."""
+
+  def __setitem__(self, key, value):
+    dict.__setitem__(self, key.lower(), value)
+
+  def __getitem__(self, key):
+    return dict.__getitem__(self, key.lower())
+
+
+class Request(object):
+  """A single HTTP request."""
+
+  def __init__(self, request, remote_addr):
+    self.remote_addr = remote_addr
+    lines = request.split('\r\n')
+    verb_line = lines.pop(0)
+    method, path, unused_protocol = verb_line.split(' ', 2)
+
+    self.headers = CaseInsensitiveDict()
+    for line in lines:
+      key, value = line.split(': ', 1)
+      self.headers[key] = value
+
+    self.url = 'http://%s/%s' % (self.headers['host'], path.lstrip('/'))
+
+    self.method = method.upper()
+    self.path = path
+    self.query_string = ''
+    if '?' in self.path:
+      self.path, self.query_string = self.path.split('?', 1)
+
+    self._qs = urlparse.parse_qs(self.query_string)
+    # TODO(flamingcow): Cookies.
+
+  def get(self, argument_name, default_value=''):
+    """Get one value of a query argument by name."""
+    return self._qs.get(argument_name, [default_value])[0]
+
+  def get_all(self, argument_name):
+    """Get all values of a query argument by name."""
+    return self._qs.get(argument_name, [])
+
+
+class OutFile(object):
+  """Chunked HTTP encoding for response output."""
+
+  def __init__(self, connection):
+    self._connection = connection
+
+  def write(self, value):
+    try:
+      self._connection.send('%X\r\n' % len(value))
+      self._connection.send(value)
+      logging.debug('Server wrote content of length %s', len(value))
+      self._connection.send('\r\n')
+    except IOError:
+      self._connection.close()
+
+
+class Response(object):
+  """Response API for a single request."""
+
+  def __init__(self, connection):
+    self._connection = connection
+    self.headers = headers.Headers([('Transfer-Encoding', 'chunked')])
+    self.out = OutFile(self._connection)
+
+  def start_response(self, code):
+    """Send code and headers. Must be run before using Response.out."""
+    try:
+      self._connection.send('HTTP/1.1 %d %s\r\n' %
+                            (code, ResponseCode.GetReasonPhrase(code)))
+      self._connection.send(str(self.headers))
+    except IOError:
+      self._connection.close()
+
+  def end_response(self):
+    """Finalize response. Must be run after all other operations."""
+    self.out.write('')
+    self._connection.ResponseComplete()
+
+
+class ResponseCode(object):
+  """API for retrieval of HTTP status code phrases."""
+  _RESPONSE_CODES = {
+      # Informational
+      100: 'Continue',                          # HTTP/1.1, RFC 2616
+      101: 'Switching Protocols',               # HTTP/1.1, RFC 2616
+      102: 'Processing',                        # WEBDAV, RFC 2518
+      # Successful
+      200: 'OK',                                # HTTP/1.1, RFC 2616
+      201: 'Created',                           # HTTP/1.1, RFC 2616
+      202: 'Accepted',                          # HTTP/1.1, RFC 2616
+      203: 'Non-Authoritative Information',     # HTTP/1.1, RFC 2616
+      204: 'No Content',                        # HTTP/1.1, RFC 2616
+      205: 'Reset Content',                     # HTTP/1.1, RFC 2616
+      206: 'Partial Content',                   # HTTP/1.1, RFC 2616
+      207: 'Multi-Status',                      # WEBDAV, RFC 2518
+      226: 'IM Used',                           # RFC 3229
+      # Redirection
+      300: 'Multiple Choices',                  # HTTP/1.1, RFC 2616
+      301: 'Moved Permanently',                 # HTTP/1.1, RFC 2616
+      302: 'Found',                             # HTTP/1.1, RFC 2616
+      303: 'See Other',                         # HTTP/1.1, RFC 2616
+      304: 'Not Modified',                      # HTTP/1.1, RFC 2616
+      305: 'Use Proxy',                         # HTTP/1.1, RFC 2616
+      307: 'Temporary Redirect',                # HTTP/1.1, RFC 2616
+      # Client Error
+      400: 'Bad Request',                       # HTTP/1.1, RFC 2616
+      401: 'Unauthorized',                      # HTTP/1.1, RFC 2616
+      402: 'Payment Required',                  # HTTP/1.1, RFC 2616
+      403: 'Forbidden',                         # HTTP/1.1, RFC 2616
+      404: 'Not Found',                         # HTTP/1.1, RFC 2616
+      405: 'Method Not Allowed',                # HTTP/1.1, RFC 2616
+      406: 'Not Acceptable',                    # HTTP/1.1, RFC 2616
+      407: 'Proxy Authentication Required',     # HTTP/1.1, RFC 2616
+      408: 'Request Timeout',                   # HTTP/1.1, RFC 2616
+      409: 'Conflict',                          # HTTP/1.1, RFC 2616
+      410: 'Gone',                              # HTTP/1.1, RFC 2616
+      411: 'Length Required',                   # HTTP/1.1, RFC 2616
+      412: 'Precondition Failed',               # HTTP/1.1, RFC 2616
+      413: 'Request Entity Too Large',          # HTTP/1.1, RFC 2616
+      414: 'Request-URI Too Long',              # HTTP/1.1, RFC 2616
+      415: 'Unsupported Media Type',            # HTTP/1.1, RFC 2616
+      416: 'Requested Range Not Satisfiable',   # HTTP/1.1, RFC 2616
+      417: 'Expectation Failed',                # HTTP/1.1, RFC 2616
+      422: 'Unprocessable Entity',              # WEBDAV, RFC 2518
+      423: 'Locked',                            # WEBDAV, RFC 2518
+      424: 'Failed Dependency',                 # WEBDAV, RFC 2518
+      426: 'Upgrade Required',                  # RFC 2817
+      # Server Error
+      500: 'Internal Server Error',             # HTTP/1.1, RFC 2616
+      501: 'Not Implemented',                   # HTTP/1.1, RFC 2616
+      502: 'Bad Gateway',                       # HTTP/1.1, RFC 2616
+      503: 'Service Unavailable',               # HTTP/1.1, RFC 2616
+      504: 'Gateway Timeout',                   # HTTP/1.1, RFC 2616
+      505: 'HTTP Version Not Supported',        # HTTP/1.1, RFC 2616
+      507: 'Insufficient Storage',              # WEBDAV, RFC 2518
+      510: 'Not Extended',                      # RFC 2774
+  }
+
+  @classmethod
+  def GetReasonPhrase(cls, code):
+    """Returns the W3C name for a given response code."""
+    if not isinstance(code, int):
+      raise TypeError('HTTP status code must be an integer: %r' % code)
+
+    if code not in cls._RESPONSE_CODES:
+      logging.warn('Unknown HTTP status code: %d', code)
+    return cls._RESPONSE_CODES.get(code, 'Response')
+
+
+class Connection(asyncore.dispatcher_with_send):
+  """A client connection."""
+
+  _blocked = False
+
+  def __init__(self, sock, addr, callback):
+    asyncore.dispatcher_with_send.__init__(self, sock)
+    self._buffer = ''
+    self._remote_addr = addr
+    self._callback = callback
+
+  def handle_read(self):
+    data = self.recv(8192)
+    if not data:
+      self.close()
+      return
+    self._buffer += data
+    self._CheckForNewRequest()
+
+  def _CheckForNewRequest(self):
+    """Check the buffer for a new request."""
+    if self._blocked:
+      return
+    if '\r\n\r\n' in self._buffer:
+      request, new_buffer = self._buffer.split('\r\n\r\n', 1)
+      # TODO(flamingcow): Support requests with body contents.
+      self._buffer = new_buffer
+      response = Response(self)
+      try:
+        request = Request(request, self._remote_addr)
+      except (KeyError, ValueError):
+        response.headers.add_header('Content-Type', 'text/plain')
+        response.start_response(400)
+        response.out.write('Failed to parse request.')
+        response.end_response()
+        return
+
+      if request.path != '/favicon.ico':
+        logging.info('[%s] %s %s', self._remote_addr, request.method,
+                     request.path)
+      self._blocked = True
+      self._callback(request, response)
+
+  def ResponseComplete(self):
+    """Associated request completed, so we can dispatch another one."""
+    self._blocked = False
+    self._CheckForNewRequest()
+
+
+class Server(asyncore.dispatcher):
+  """HTTP server that dispatches tasks for asynchronous responses."""
+
+  def __init__(self, host, port, callback):
+    asyncore.dispatcher.__init__(self)
+    self._callback = callback
+    self._started = threading.Event()
+    self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.set_reuse_addr()
+    self.bind((host, port))
+    self.listen(5)
+    self._connections = weakref.WeakValueDictionary()
+
+  def handle_accept(self):
+    pair = self.accept()
+    if pair is None:
+      return
+    sock, addr = pair
+    logging.debug('Received connection from %s:%s' % addr)
+    self._connections[sock] = Connection(sock, addr[0], self._callback)
+
+  def WaitUntilHTTPServerIsRunning(self):
+    self._started.wait()
+
+  def Loop(self):
+    self._started.set()
+    asyncore.loop()
+
+  def Shutdown(self):
+    for conn in self._connections.values():
+      if conn is not None:
+        conn.close()
+    self.close()
