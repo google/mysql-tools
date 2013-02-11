2c89e0d58b8c22b5f93cc827296c032732db69b6
flamingcow@google.com <flamingcow@google.com@960bbd1a-3921-0410-93bb-a35bc832efea>
2013-02-11 20:05:44 +0000

    Lint cleanup.
    git-svn-id: http://google-mysql-tools.googlecode.com/svn/trunk@354 960bbd1a-3921-0410-93bb-a35bc832efea

--- permissions-example.py
+++ permissions-example.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python2.6
-#
-# Copyright 2011 Google Inc.
+# Copyright 2011 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+"""An example permissions file, demonstrating roles through inheritance."""
 
 __author__ = 'flamingcow@google.com (Ian Gulliver)'
 
